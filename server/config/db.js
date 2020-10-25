779213967051b20dc7ac662a55b562ef36fa6ebd
Kjumar <jganguli@hotmail.ca>
2020-10-25 19:54:03 -0400

    database URI now directs to Online Database

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/portfolio"
+    // "URI": "mongodb://localhost/portfolio"
+    "URI": "mongodb+srv://jay:KTbMewNQDMKXKIwp@mongodbserver.sd61v.mongodb.net/portfolio?retryWrites=true&w=majority"
 }
